88c9602fa1cdf3888a82619c3adbebe53458a715
David Smith <davidsmith2@gmail.com>
2016-11-15 15:30:29 -0500

    Stand up the basic server/db

---
+++ server.js
@@ -0,0 +1,194 @@
+/* ==========================================================================================
+ imports
+ ========================================================================================== */
+
+var bodyParser = require('body-parser');
+var cors = require('express-cors');
+var errorHandler = require('errorhandler');
+var express = require('express');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var path = require('path');
+var uriUtil = require('mongodb-uri');
+
+/* ==========================================================================================
+ config
+ ========================================================================================== */
+
+var config = {
+    app: {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: 4711,
+        cors: {
+            allowedOrigins: ['http://localhost:8080']
+        }
+    },
+    db: {
+        options: {
+            server: {
+                socketOptions: {
+                    keepAlive: 1,
+                    connectTimeoutMS: 30000
+                }
+            },
+            replset: {
+                socketOptions: {
+                    keepAlive: 1,
+                    connectTimeoutMS: 30000
+                }
+            }
+        },
+        url: uriUtil.formatMongoose('mongodb://admin:admin@ds155097.mlab.com:55097/numbers-up')
+    }
+};
+
+/* ==========================================================================================
+ db setup
+ ========================================================================================== */
+
+var Guesses = new mongoose.Schema({
+    guess: Number
+});
+
+var Game = new mongoose.Schema({
+    guessesAllowed: Number,
+    guessesMade: Number,
+    result: String,
+    secretNumber: Number
+});
+
+var Player = new mongoose.Schema({
+    inid: String,
+    firstName: String,
+    lastName: String
+});
+
+var GameModel = mongoose.model('Game', Game);
+
+var PlayerModel = mongoose.model('Player', Player);
+
+mongoose.connection.on('error', console.error.bind(console, 'Mongoose connection error'));
+mongoose.connection.once('open', console.log.bind(console, 'Mongoose connection open'));
+mongoose.connect(config.db.url, config.db.options);
+
+/* ==========================================================================================
+ app setup
+ ========================================================================================== */
+
+var app = express();
+
+app.use(
+    express.static(
+        path.join(
+            __dirname, 'app'
+        )
+    )
+);
+
+app.use('/js/lib/', express.static('node_modules/requirejs/'));
+
+app.use('/node_modules', express.static('node_modules'));
+
+app.use('/test', express.static('test/'));
+
+app.use('/test', express.static('app'));
+
+app.use(cors(config.app.cors));
+
+app.use(bodyParser.json());
+
+app.use(methodOverride());
+
+app.use(errorHandler());
+
+/* ==========================================================================================
+ routes
+ ========================================================================================== */
+
+app.get('/api/games', function (request, response) {
+    return GameModel.find(function (error, games) {
+        if (!error) {
+            return response.send(games);
+        } else {
+            return console.log(error);
+        }
+    });
+});
+
+app.post('/api/games', bodyParser.json(), function (request, response) {
+    var game = new GameModel(request.body);
+    game.save(function (error, game) {
+        if (error) {
+            return console.log(error);
+        }
+        response.json(game)
+    });
+});
+
+app.get('/api/players', function (request, response) {
+    return PlayerModel.find(function (error, players) {
+        if (!error) {
+            return response.send(players);
+        } else {
+            return console.log(error);
+        }
+    });
+});
+
+app.post('/api/players', function (request, response) {
+    var player = new PlayerModel({
+        inid: request.body.inid,
+        firstName: request.body.firstName,
+        lastName: request.body.lastName
+    });
+
+    player.save(function (error) {
+        if (!error) {
+            return console.log('player created');
+        } else {
+            return console.log(error);
+        }
+    });
+
+    return response.send(player);
+
+});
+
+app.put('/api/players/:id', function (request, response) {
+    return PlayerModel.findById(request.params.id, function (err, player) {
+        player.firstName = request.body.firstName;
+        player.lastName = request.body.lastName;
+        return player.save(function (err) {
+            if (!err) {
+                console.log('player updated');
+            } else {
+                console.log(err);
+            }
+            return response.send(player);
+        });
+    });
+});
+
+app['delete']('/api/players/:id', function (request, response) {
+    return PlayerModel.findById(request.params.id, function (err, player) {
+        return player.remove(function (err) {
+            if (!err) {
+                console.log('player deleted');
+                return response.send('');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+});
+
+/* ==========================================================================================
+ start the server
+ ========================================================================================== */
+
+app.listen(config.app.port, function(){
+    var url = config.app.protocol + '://' + config.app.hostname + ':' + config.app.port;
+    var message = 'Express server listening on port %d in %s mode. Application running at ' + url + '.';
+    console.log(message, config.app.port, app.settings.env);
+});
